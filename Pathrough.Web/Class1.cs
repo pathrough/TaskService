57991df9b2d8f5ef10c0874ec6ff203b028c8737
pathrough <1598842213@qq.com>
2014-12-02 16:15:17 +0800

    添加项目

---
+++ Pathrough.Web/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Pathrough.Web
+{
+    public class DataCache
+    {
+        /// <summary>
+        /// 获取当前应用程序指定CacheKey的Cache值--韩义
+        /// </summary>
+        public static object GetCache(string CacheKey)
+        {
+            System.Web.Caching.Cache objCache = HttpRuntime.Cache;
+            return objCache[CacheKey];
+        }
+
+        /// <summary>
+        /// 设置当前应用程序指定CacheKey的Cache值--韩义
+        /// </summary>
+        public static void SetCache(string CacheKey, object objObject)
+        {
+            System.Web.Caching.Cache objCache = HttpRuntime.Cache;
+            objCache.Insert(CacheKey, objObject);
+        }
+    }
+}
