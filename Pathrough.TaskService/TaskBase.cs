46b2beb42a989f855f0791f0640e14416d13e52f
pathrough <1598842213@qq.com>
2014-12-02 12:59:56 +0800

    TaskBase

---
+++ Pathrough.TaskService/TaskBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pathrough.TaskService
+{
+   
+    public abstract class TaskBase
+    {
+        public TaskBase()
+        {
+            RunEnabled = true;
+        }
+        public void StartRun()
+        {
+            while (true)
+            {
+                if (AbortBeforeNextRun)
+                {
+                    Console.WriteLine("Abort:" + this.GetType());
+                    Thread.CurrentThread.Abort();
+                }
+                else
+                {
+                    var now = DateTime.Now;
+                    if (RunEnabled && RunSartTime < now && RunEndTime > now)
+                    {
+                        Console.WriteLine("Run:" + this.GetType());
+                        Run();
+                    }
+                    else
+                    {
+                        Console.WriteLine("NotRun:" + this.GetType());
+                    }
+                    Thread.Sleep(this.RunPeriod);
+                }
+               
+            };
+        }
+        protected abstract void Run();
+        protected abstract DateTime RunSartTime { get; }
+        protected abstract DateTime RunEndTime { get; }
+        public abstract int RunPeriod { get; }
+        public bool RunEnabled { get; set; }
+
+        public bool AbortBeforeNextRun { get; set; }
+
+    }
+
+    
+}
