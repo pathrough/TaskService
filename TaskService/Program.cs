b0bd9ca20a78c14db34fc01617a9d113a8a57a5a
pathrough <1598842213@qq.com>
2014-12-04 13:13:35 +0800

    Ninject依赖注入

--- TaskService/Program.cs
+++ TaskService/Program.cs
@@ -12,6 +12,17 @@ namespace TaskService
         static void Main(string[] args)
         {
             TaskManager.Current.Init();
+
+            Dictionary<string, string> patern = new Dictionary<string, string>{
+                {"{obj}和{obj}有很大的不同",""},
+                {"对于{obj}的情形",""},
+                {"在{}时",""},
+                {"首先，{}","首先，如果你家庭条件负担得起的话，一定要和麻麻申请购买衣服的资金！"},
+                {"如果{}的话，{}","如果你家庭条件负担得起的话，一定要和麻麻申请购买衣服的资金！"}
+                
+                
+            };
+            string instance = "精算和风险管理 有很大的不同。";
         }
     }
 }
