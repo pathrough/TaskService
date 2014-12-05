2e44729e23fd843a5cf16b6d9042d7ec6b1b2b36
pathrough <1598842213@qq.com>
2014-12-05 14:16:01 +0800

    HtmlToOpenXml

---
+++ HtmlToOpenXmlConsoleApplication1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using NotesFor.HtmlToOpenXml;
+
+namespace HtmlToOpenXmlConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string filename = "test.docx";
+            string html = File.ReadAllText("htmlFile.html");
+
+            if (File.Exists(filename)) File.Delete(filename);
+
+            using (MemoryStream generatedDocument = new MemoryStream())
+            {
+                using (WordprocessingDocument package = WordprocessingDocument.Create(generatedDocument, WordprocessingDocumentType.Document))
+                {
+                    MainDocumentPart mainPart = package.MainDocumentPart;
+                    if (mainPart == null)
+                    {
+                        mainPart = package.AddMainDocumentPart();
+                        new Document(new Body()).Save(mainPart);
+                    }
+
+                    HtmlConverter converter = new HtmlConverter(mainPart);
+                    Body body = mainPart.Document.Body;
+
+                    var paragraphs = converter.Parse(html);
+                    for (int i = 0; i < paragraphs.Count; i++)
+                    {
+                        body.Append(paragraphs[i]);
+                    }
+
+                    mainPart.Document.Save();
+                }
+
+                File.WriteAllBytes(filename, generatedDocument.ToArray());
+            }
+
+            System.Diagnostics.Process.Start(filename);
+        }
+    }
+}
