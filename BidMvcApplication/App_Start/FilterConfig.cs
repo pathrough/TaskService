210d1a77e66a0d3d39d7a764bdc5ce030c117995
pathrough <1598842213@qq.com>
2015-02-11 00:53:15 +0800

    dfdf

--- BidMvcApplication/App_Start/FilterConfig.cs
+++ BidMvcApplication/App_Start/FilterConfig.cs
@@ -10,4 +10,59 @@ namespace BidMvcApplication
             filters.Add(new HandleErrorAttribute());
         }
     }
+
+    public enum UserType
+    {
+        Common,
+        Admin
+    }
+
+    public class UserAuthorizeAttribute : FilterAttribute, IAuthorizationFilter
+    {
+        public UserAuthorizeAttribute(UserType userType)
+        {
+
+        }
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            var userInfo = filterContext.HttpContext.Session[UserConfig.SESSION_NAME];
+            if (userInfo == null)
+            {
+                filterContext.Result = new HttpUnauthorizedResult(); //返回未授权Result 
+                //跳转到登录页面 
+                filterContext.HttpContext.Response.Redirect("/User/Login");
+            }
+        }
+    }
+
+    public interface IUserView
+    {
+        void UserNoOrPasswordInvlid();
+        void LoginSuccess();
+    }
+
+    public class UserAuthorize
+    {
+        public static void Login(string userNo,string userPwd,IUserView view)
+        {
+            if (userNo == "wangyb" && userPwd == "123456")
+            {
+                HttpContext.Current.Session[UserConfig.SESSION_NAME]=userNo;
+                view.LoginSuccess();
+            }           
+            else
+            {
+                view.UserNoOrPasswordInvlid();
+            }
+        }
+        public static void Logout()
+        {
+            HttpContext.Current.Session[UserConfig.SESSION_NAME] = null;
+        }
+    }
+
+    public class UserConfig
+    {
+        public const string SESSION_NAME="userInfo";
+    }
 }
---
+++ BidMvcApplication/Controllers/UserController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BidMvcApplication.Controllers
+{
+    public class UserController : Controller, IUserView
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Login(string userNo,string userPwd)
+        {
+            if(Request.Form.Count>0 | Request.QueryString.Count>0)
+            {
+                UserAuthorize.Login(userNo, userPwd, this);
+            }            
+            return View();
+        }
+
+
+        //
+        // GET: /User/Details/5
+        [UserAuthorize(UserType.Common)]
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /User/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /User/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /User/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /User/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /User/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /User/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public void UserNoOrPasswordInvlid()
+        {
+            Response.Write("用户名或密码错误！");
+            Response.End();
+        }
+
+        public void LoginSuccess()
+        {
+            Response.Write("登录成功");
+            Response.End();
+        }
+    }
+}
