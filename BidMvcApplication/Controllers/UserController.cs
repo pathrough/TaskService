4ac1fa4ed2ba9837e9a5ed9d24c290c308c51eaf
pathrough <1598842213@qq.com>
2015-02-13 01:41:25 +0800

    zxcx

--- BidMvcApplication/Controllers/UserController.cs
+++ BidMvcApplication/Controllers/UserController.cs
@@ -9,15 +9,7 @@ using System.Web.Mvc;
 namespace BidMvcApplication.Controllers
 {
     public class UserController : Controller, IUserView
-    {
-        //
-        // GET: /User/
-
-        public ActionResult Index()
-        {
-            return View();
-        }
-        
+    {       
         public ActionResult Login(string userNo,string userPwd)
         {
             if (!string.IsNullOrWhiteSpace(userNo) && !string.IsNullOrWhiteSpace(userPwd))
@@ -27,102 +19,17 @@ namespace BidMvcApplication.Controllers
             return View();
         }
 
-
-        //
-        // GET: /User/Details/5
-        [UserAuthorize(UserType.Common)]
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        //
-        // GET: /User/Create
-
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        //
-        // POST: /User/Create
-
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /User/Edit/5
-
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /User/Edit/5
-
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /User/Delete/5
-
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /User/Delete/5
-
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
         public void UserNoOrPasswordInvlid()
         {
-            Response.Write(new JsonResultEntity { Result = 2, Msg = "用户名或密码错误！" }.ToJson());
+            Json(new JsonResultEntity { Result = 2, Msg = "用户名或密码错误！" }).ExecuteResult(this.ControllerContext);
+            //Response.Write(new JsonResultEntity { Result = 2, Msg = "用户名或密码错误！" }.ToJson());
             Response.End();
         }
 
         public void LoginSuccess()
         {
-            Response.Write(new JsonResultEntity { Result = 1, Msg = "登录成功！" }.ToJson());
+            Json(new JsonResultEntity { Result = 1, Msg = "登录成功！" }).ExecuteResult(this.ControllerContext);
+            //Response.Write();
             Response.End();
         }
     }
